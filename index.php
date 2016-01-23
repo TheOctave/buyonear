b8e2d0b86f86d9695f0bb0baef093debec3cf8bd
Ike <iasomugha2010@my.fit.edu>
2016-01-23 17:19:56 -0500

    index.php and other files
    Access points from index.php were created

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+$url = isset($_GET['url']) ? $_GET['url'] : 'error';
+$url = rtrim($url, '/');
+$urlArray = explode("/", $url);
+
+if ($urlArray[0] == 'register') {
+    
+    include 'controls/register/index.php';
+} else if ($urlArray[0] == 'products') {
+    
+	if (!isset($urlArray[1]) ) {
+		
+		include 'controls/products/index.php';
+	} else if($urlArray[1]) == "id") {
+		
+		if (isset($urlArray[2])) {
+		
+			if ($urlArray[2] == "rate") {
+				include 'controls/products/rate.php';
+			} else if ($urlArray[2] == "bid"){
+				include 'controls/products/bid.php';
+			} else {
+				basicFailureStatus();
+			}
+		} else {
+			
+			//display the product
+		}
+	}
+} else if ($urlArray[0] == 'profile') {
+    
+    include 'controls/product.php';
+}else if ($urlArray[0] == 'search') {
+ 
+    include 'controls/search.php';
+} else {
+	
+	//Error page
+}
+
+function basicFailureStatus() {
+	
+	$result = array('status' => 'failure');
+	echo json_encode($result);
+	die;
+}
